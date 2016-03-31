1e3cb925ee79ada4f4bd4adb51b90c6ee17bf78b
zanovid <zanovid@gmail.com>
2016-03-31 09:05:42 -0400

    First commit, set variables for name, number, and amount.

--- src/main/business/Card.java
+++ src/main/business/Card.java
@@ -2,6 +2,12 @@ package main.business;
 
 /**
  * Created by DM53848 on 3/31/2016.
+ * @author damirzanovic
  */
 public class Card {
+	String name="";
+	int accountNumber=0;
+	double amount=0;
+	
+	
 }
