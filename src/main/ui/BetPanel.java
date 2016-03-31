b5ef64f811935b15dec77fe269efb3e5da359e22
mmosser <mmosser@gaig.com>
2016-03-31 10:36:59 -0400

    Updates to bet panel

--- src/main/ui/BetPanel.java
+++ src/main/ui/BetPanel.java
@@ -1,7 +1,21 @@
 package main.ui;
 
+import main.business.BetType;
+
 /**
  * Created by DM53848 on 3/31/2016.
  */
 public class BetPanel {
+	
+private BetType betSelection;
+	
+public void setBet(BetType betSelection){
+	this.betSelection = betSelection; 
+}
+
+public void Spin(){
+	
+	
+}
+
 }
