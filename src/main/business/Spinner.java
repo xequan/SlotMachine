afa34aec7a3af67e21c56497a60a48477977f558
Joey Squires <joey.squires@kroger.com>
2016-04-28 10:13:22 -0400

    Updated Spinner to use Thread and Displayable
    Added JTextField and made the run method start each spinner. JTextField needs to implement getter and setter.

--- src/main/business/Spinner.java
+++ src/main/business/Spinner.java
@@ -1,25 +1,68 @@
 package main.business;
 
+import javax.swing.JTextField;
+
 import main.math.RNG;
+import main.ui.util.Displayable;
 
 /**
  * Created by DM53848 on 3/31/2016.
  */
-public class Spinner {
-    private int showingFace = 0;
-    RNG randomNumberGenerator;
+public class Spinner extends Thread implements Displayable{
+	private int showingFace = 0;
+	RNG randomNumberGenerator;
+	private JTextField spinnerField;
+	private int runTime;
 
-    public Spinner() {
-        this.randomNumberGenerator = new RNG();
-        this.randomNumberGenerator.setRange(1, 3);
+	public int getRunTime() {
+		return runTime;
+	}
 
-    }
+	public void setRunTime(int runTime) {
+		this.runTime = runTime;
+	}
 
-    public void spin() {
-        this.showingFace = randomNumberGenerator.getRandom();
-    }
+	public JTextField getSpinnerField() {
+		return spinnerField;
+	}
 
-    public int getShowingFace() {
-        return this.showingFace;
-    }
+	public void setSpinnerField(JTextField spinnerField) {
+		this.spinnerField = spinnerField;
+	}
+
+	public Spinner() {
+		this.randomNumberGenerator = new RNG();
+		this.randomNumberGenerator.setRange(1, 3);
+
+	}
+
+	public void run() {
+		int counter = 0;
+		while(true){
+			for(; counter < 20; counter++){
+				showingFace = (int)randomNumberGenerator.getRandom();
+				display(showingFace + "");
+				try{
+					Thread.sleep(runTime);
+
+				}
+				catch(InterruptedException e){
+
+				}
+			}
+			if(counter == 20){
+				break;
+			}
+		}
+	}
+
+	public int getShowingFace() {
+		return this.showingFace;
+	}
+
+	@Override
+	public void display(String display) {
+		spinnerField.setText(display);
+
+	}
 }
